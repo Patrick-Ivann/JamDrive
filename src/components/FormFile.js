1e0f4a6765cc136166a64266eea11a39ede3b8de
ShinobuFormula <damien.laporte@viacesi.fr>
2018-12-15 17:55:26 +0100

    Update -> Form addfile
    Now we can add file
    to do -> make the whole popup responsive

--- src/components/FormFile.js
+++ src/components/FormFile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import cross from '../image/cross.png'
-
+import addFileHere from '../image/addFileHere.png'
 
 class FormFile extends Component {
 
@@ -10,16 +10,17 @@ render() {
   return (
     <div id="opacity">
       <div id="formulaire">
+
       <img id="closeButton" src={cross} onClick={this.props.onClick} alt="close button"/>
       <h1>Nouveau Fichier</h1>
-
+      <hr/>
       <form>
         <label id="formUE">  UE
-            <input type="checkbox" name="UE" value="Science"/> Sciences de base
-            <input type="checkbox" name="UE" value="Wen"/> Développement Web
-            <input type="checkbox" name="UE" value="Humain"/> Sciences humaines <br/>
-            <input type="checkbox" name="UE" value="Meca"/> Mécanique
-            <input type="checkbox" name="UE" value="Reseaux"/> Architectures réseaux<br/> </label>
+            <input type="radio" name="UE" value="Science"/> Sciences de base
+            <input type="radio" name="UE" value="Wen"/> Développement Web
+            <input type="radio" name="UE" value="Humain"/> Sciences humaines <br/>
+            <input type="radio" name="UE" value="Meca"/> Mécanique
+            <input type="radio" name="UE" value="Reseaux"/> Architectures réseaux<br/> </label>
         <label id="formProsit">  Nom du prosit
           <input type="text" name="Nom" /> </label>
         <br/>
@@ -30,6 +31,9 @@ render() {
             <input type="checkbox" name="checkType" value="Classe"/><br/> </label>
         <label>  Validé par le tuteur
             <input type="checkbox" name="checkType" value="Tuteur"/> </label>
+        <label id="lblFile" htmlFor="file"> Ajouter le fichier <br/> <img id="addFileHere" src={addFileHere} alt="add file here !"/> </label>
+            <input type="file" name="file" id="file" accept=".doc,.docx"/>
+
       </form>
       </div>
     </div>
