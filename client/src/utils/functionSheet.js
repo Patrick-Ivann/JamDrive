384871cbc4f0c10635a21cc301946f0c4e276295
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2018-12-20 17:55:09 +0100

    installation des dépendances avant la mise en place de REDUX

---
+++ client/src/utils/functionSheet.js
@@ -0,0 +1,13 @@
+import moment from "moment";
+const sleep = milliseconds => {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+
+
+export default sleep;
