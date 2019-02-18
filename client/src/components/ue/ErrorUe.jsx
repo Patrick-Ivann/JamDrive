bff72812a1e31b02272d9743df0d6fb54ee4d4f9
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-02-18 16:04:23 +0100

    fix

--- client/src/components/ue/ErrorUe.jsx
+++ client/src/components/ue/ErrorUe.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 export default function ErrorUe() {
   return (
-    <div>
+    <div class="container">
+    <div class="row">
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4">
+
       <h1 className="mx-auto" >Aucun prosit à afficher</h1>
+
     </div>
+      </div>
+      </div>      
   )
 }
 
