840484fad8f8e0d3bc1ecb6312f5f2cfeeace7d3
juliengriffoul <julien.griffoul@viacesi.fr>
2019-03-16 19:34:34 +0100

    :pencil2: Update godmode prosit component

--- client/src/components/ue/FormulaireFichier.jsx
+++ client/src/components/ue/FormulaireFichier.jsx
@@ -61,9 +61,8 @@ class FormulaireFichier extends Component {
                         <label as="label" id={`lblFile${this.state.id}`} htmlFor={`fileInput${this.state.id}`} className="btn btn-secondary btn-lg btn-block text-left">Ajouter  {(this.props.prositID) ? "une ressource" : " un prosit " + this.props.prositType} </label>
                         <input type="file" style={{ display: "none" }} onChange={(event) => this.getFileName(event)} name="file" ref="getFile" id={`fileInput${this.state.id}`} accept=".doc,.docx,.txt,.pdf" />
                       </div>
-                      <div className="col-5"> </div>
-                      <div className="col-2">
-                        <input className="btn btn-success btn-lg btn-block mb-2 text-left pl-2" type="submit" />
+                      <div className="col-3">
+                        <input className="btn btn-success btn-lg btn-block mb-2 text-center" type="submit" />
                       </div>
                     </div>
                 ):(
