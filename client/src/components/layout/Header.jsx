6644f0f035226c772f93f8af046e783777053692
juliengriffoul <julien.griffoul@viacesi.fr>
2019-03-16 18:11:53 +0100

    :pencil2: Update prosit component rendering and clean code

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -4,19 +4,30 @@ import { connect } from 'react-redux'
 import {mettreAjourRecherche} from '../../actions/prositActions';
 import { changerTheme } from "../../actions/themeAction";
 import { logoutUser } from "../../actions/authAction";
-
 import { navigatorCheck } from '../../utils/functionSheet';
+
+/**
+ *
+ */
 class Header extends Component {
 
+  /**
+   *
+   * @param props
+   */
   constructor(props) {
     super(props);
     this.state = {
       recherche: ''
-    }
-    this.handleSearch = this.handleSearch.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  /**
+   *
+   * @param event
+   */
   handleChange(event) {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
@@ -26,107 +37,80 @@ class Header extends Component {
       [name]: value
     });
 
-    this.props.mettreAjourRecherche(value)
-
-
-
-
+    this.props.mettreAjourRecherche(value);
   }
 
-  handleClick(event){
-    event.preventDefault()
+  /**
+   *
+   * @param event
+   */
+  handleClick(event) {
+    event.preventDefault();
 
     this.props.history.push({
       pathname: '/',
       state: {
         from: this.props.location.pathname
       }
-    })  
-  
-  
-    window.location.href = '/'
+    });
 
+    window.location.href = '/';
   }
 
-
-  
-
+  /**
+   *
+   * @param event
+   */
   handleSearch(event) {
-   // this.setState({ recherche: event.target.value.substr(0, 20) })
-    
-   this.setState({ recherche: event.target.value })
-
-   this.props.mettreAjourRecherche(event.target.value)
+    // this.setState({ recherche: event.target.value.substr(0, 20) })
+    this.setState({ recherche: event.target.value });
+    this.props.mettreAjourRecherche(event.target.value);
   }
 
+  /**
+   *
+   * @returns {*}
+   */
+  render() {
+    let fileImg = null;
+    if (navigatorCheck() === "Safari" || navigatorCheck() === "IE" || navigatorCheck() === "unknown") {
+      fileImg = require("../../static/image/jampops.png");
+    } else {
+      fileImg = require("../../static/image/jampops.webp");
+    }
 
+    return (
+        <header className={"mb-4"}>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <img onClick={ (event) => { this.handleClick(event); this.props.logoutUser() } } className="logo mr-2" src={fileImg} alt="Logo" width="100px" />
+            <div className="navbar-brand">JAMDRIVE</div>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"> </span>
+            </button>
 
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              </ul>
+              <form className="form-inline my-2 my-lg-0">
+                <label aria-label="recherche" id="lblRecherche" htmlFor="recherche"> &nbsp;</label>
 
-render() {
+                <input className="form-control mr-sm-2" id="recherche" name='recherche' value={this.state.recherche}
+                       onChange={this.handleChange} type="search" placeholder="Recherche.."/>
+              </form>
 
-  
-
-
-  let fileImg = null
-
-  if (navigatorCheck() === "Safari" || navigatorCheck() === "IE" || navigatorCheck() === "unknown") {
-
-    fileImg = require("../../static/image/jampops.png")
-
-  } else {
-
-    fileImg = require("../../static/image/jampops.webp")
-  }
-
-  
-  
-  return (
-      <header className={"mb-4"}>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img onClick={(event) => { this.handleClick(event); this.props.logoutUser()} } className="logo mr-2" src={fileImg} alt="Logo" width="100px" />
-          <div className="navbar-brand">JAMDRIVE </div>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
-                  aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            </ul>
-            <form className="form-inline my-2 my-lg-0">
-            <label aria-label="recherche" id="lblRecherche"htmlFor="recherche"> &nbsp;</label>
-
-              <input className="form-control mr-sm-2" id="recherche" name='recherche' value={this.state.recherche}
-                     onChange={this.handleChange} type="search" placeholder="Recherche.."/>
-            </form>
-
-            <button className="btn btn-primary my-2 mr-2 my-sm-0" data-toggle="modal" data-target="#fileModal">Nouveau fichier</button>
-            <button className="btn btn-secondary my-2 my-sm-0" onClick={this.props.changerTheme}>Thème {(this.props.theme.theme) ? "clair" : "sombre"} </button>
-          </div>
-        </nav>
-      </header>
+              <button className="btn btn-primary my-2 mr-2 my-sm-0" data-toggle="modal" data-target="#fileModal">Nouveau fichier</button>
+              <button className="btn btn-secondary my-2 my-sm-0" onClick={this.props.changerTheme}>Thème {(this.props.theme.theme) ? "clair" : "sombre"} </button>
+            </div>
+          </nav>
+        </header>
     );
   }
 }
-/*
-
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-              </li>
- */
 
 const mapStateToProps = state => ({
-
   prosit: state.prosit,
   theme: state.theme
-
-})
-
+});
 
 export default withRouter(connect(mapStateToProps,{mettreAjourRecherche, changerTheme, logoutUser}) (Header));
--- client/src/components/prosits/PrositItem.jsx
+++ client/src/components/prosits/PrositItem.jsx
@@ -2,76 +2,91 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import { checkerProsit } from "../../actions/prositActions";
 import { connect } from 'react-redux'
-
 import PrositStyled from '../../styles/PrositStyled';
 import FormulaireFichier from '../ue/FormulaireFichier';
-
 
 /**
  * TODO il faut trouver un moyen(un bouton) pour lancer la fonction de check et lui passer l'id à la fonction check
  * ! mettre un ternary operator pour qui vérifie la validation du prosit (1,0,2) la valeur est this.props.prosit.validation
  */
 class PrositItem extends Component {
-    constructor(props) {
-        super(props)
 
-        this.Check = this.Check.bind(this)
+    /**
+     *
+     * @param props
+     */
+    constructor(props) {
+        super(props);
+        this.Check = this.Check.bind(this);
     }
 
-    componentDidUpdate = (prevProps, prevState) => {
-      
+    /*componentDidUpdate = (prevProps, prevState) => {
         if(prevProps.prosit !== this.props.prosit){
 
         }
-    }
-    
+    }*/
 
-
-
+    /**
+     *
+     * @param event
+     * @constructor
+     */
     Check(event) {
+        event.preventDefault();
         /*if (this.props.prosit.certification === 1) {
             return <span className="classmate">✔</span>
         }
         else if (this.props.prosit.certification === 2) {
             return <span className="validated">✔</span>
         }*/
-        event.preventDefault()
-        this.props.checkerProsit(this.props.prosit.id)
-
-
+        this.props.checkerProsit(this.props.prosit.id);
     }
 
+    /**
+     *
+     * @returns {*}
+     */
     render() {
-        const {prosit} = this.props
+        const { prosit } = this.props;
         return (
             <div className="card mt-2">
-                <PrositStyled className="card-body ">
-                    <h4>Prosit - {this.props.prosit.nomProsit}</h4>
-                    <FormulaireFichier prositID={this.props.prosit.nomProsit} ></FormulaireFichier>
+                <PrositStyled className="card-body">
+                    <h4 className={"mb-4"}>Prosit - { this.props.prosit.nomProsit }</h4>
 
-                    <ul className="list__item" >
-                        {prosit.motsClef.slice(0,4).map((motsClef, index) => (<li key={index} className="">{motsClef}</li>))}
+                    <FormulaireFichier prositID={ this.props.prosit.nomProsit }> </FormulaireFichier>
+
+                    <ul className="list__item">
+                        { prosit.motsClef.slice(0,4).map((motsClef, index) => (
+                            <li key={index} className="">{motsClef}</li>)) }
                     </ul>
-                   {(prosit.aller) ? <a href={`https://api.jampops.online/api/prosit/testtelechargement/${prosit.nomProsit}/aller`}>
-                        <button type="button" className="btn btn-primary btn-lg btn-block mb-2 mt-2 text-left">Aller</button></a> : <FormulaireFichier prositType="aller" ></FormulaireFichier>}
-                    {(prosit.retour) ? <a href={`https://api.jampops.online/api/prosit/testtelechargement/${prosit.nomProsit}/retour`}>
-                        <button type="button" className="btn btn-primary btn-lg btn-block text-left">Retour</button></a> : <FormulaireFichier prositType="retour" ></FormulaireFichier>}
+                    {
+                        (prosit.aller) ? (
+                            <a href={`https://api.jampops.online/api/prosit/testtelechargement/${prosit.nomProsit}/aller`}>
+                                <button type="button" className="btn btn-primary btn-lg btn-block mb-2 mt-2 text-left">Aller</button></a>
+                            ) : (
+                            <FormulaireFichier prositType="aller"> </FormulaireFichier>
+                        )
+                    }
+                    {
+                        (prosit.retour) ? (
+                            <a href={`https://api.jampops.online/api/prosit/testtelechargement/${prosit.nomProsit}/retour`}>
+                                <button type="button" className="btn btn-primary btn-lg btn-block text-left">Retour</button></a>
+                        ) : (
+                            <FormulaireFichier prositType="retour" > </FormulaireFichier>
+                        )
+                    }
                 </PrositStyled>
             </div>
         );
     }
-
-
 }
 
 PrositItem.propTypes = {
-
     prosit: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
-
     prositRedux: state.prosit
-})
+});
 
-export default connect(mapStateToProps,{checkerProsit})(PrositItem)
+export default connect(mapStateToProps,{checkerProsit})(PrositItem);
